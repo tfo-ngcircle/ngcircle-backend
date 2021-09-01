2a36a0d0e05dea13947e7fcc4174d3b73d49ed79
Forntoh Thomas <thomasforntoh@gmail.com>
2021-09-01 16:26:48 +0200

    added custom

--- api/jobs/controllers/jobs.js
+++ api/jobs/controllers/jobs.js
@@ -1,8 +1,12 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  find: (ctx) => {
+    return strapi.query("job").find(ctx.query, ["location"]);
+  },
+};
